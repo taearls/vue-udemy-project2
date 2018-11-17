7c8654776d46edef29a003842eb5d015d9fd43c4
Tyler Earls <tyler.a.earls@gmail.com>
2018-11-17 13:15:20 -0600

    created vue components for app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-    <div class="container">
-    
-    </div>
+  <div class="container">
+    <app-header></app-header>
+    <create-quote></create-quote>
+    <quote-grid></quote-grid>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+  import Footer from './components/shared/Footer.vue';
+  import Header from './components/shared/Header.vue';
+
+  import CreateQuote from './components/Quotes/CreateQuote.vue';
+  import QuoteGrid from './components/Quotes/QuoteGrid.vue';
+  import Quote from './components/Quotes/Quote.vue';
+
+  export default {
+    data: () => {
+      return {
+        quotes: [],
+        maxQuotes: 10
+      }
+    },
+    components: {
+      appFooter: Footer,
+      appHeader: Header,
+      quoteGrid: QuoteGrid,
+      createQuote: CreateQuote,
+      quote: Quote
+    },
+  }
 </script>
 
 <style>
---
+++ src/components/Quotes/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  .panel-body { 
+    font-family: 'Arizonia', cursive;
+    font-size: 24px;
+    color: #6e6e6e;
+  }
+  .quote {
+    cursor: pointer;
+  }
+  .quote:hover {
+    background-color: #ffe2e2;
+  }
+</style>
